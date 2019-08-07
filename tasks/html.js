dde2f8becb9beb8ec8cebf7a93fdf4d426861112
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-08-07 17:14:35 +0300

    Update pages structure

--- tasks/html.js
+++ tasks/html.js
@@ -9,16 +9,16 @@ const gulpIf = require('gulp-if');
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 module.exports = () => (
-	gulp.src('app/pug/pages/**/*.pug')
-	.pipe(plumber({
-		errorHandler: notify.onError(function(err) {
-			return {
-				title: 'HTML',
-				message: err.message,
+  gulp.src('app/pug/**/*.pug')
+  .pipe(plumber({
+    errorHandler: notify.onError(function(err) {
+      return {
+        title: 'HTML',
+        message: err.message,
         sound: false
-			}
-		})
-	}))
-	.pipe(pug(gulpIf(isDevelopment, {pretty: true})))
-	.pipe(gulp.dest('public'))
+      }
+    })
+  }))
+  .pipe(pug(gulpIf(isDevelopment, {pretty: true})))
+  .pipe(gulp.dest('public'))
 );
