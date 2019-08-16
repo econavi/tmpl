eb569b43c0a7465dd942cb03a725492e92933263
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-08-16 12:47:56 +0300

    Update social icons

--- tasks/iconsSvg.js
+++ tasks/iconsSvg.js
@@ -6,46 +6,47 @@ const svgmin = require('gulp-svgmin'); // Minify SVG
 const cheerio = require('gulp-cheerio'); // manipulate XML files
 const replace = require('gulp-replace'); // String Replace
 const gulpIf = require('gulp-if');
+const plumber = require('gulp-plumber');
 
 
 module.exports = () => (
-	gulp.src('app/assets/icons/svg/**/*.svg')
-	.pipe(svgmin({
-		js2svg: {
-			pretty: true
-		}
-	}))
-	// remove all fill, style and stroke declarations in out shapes
-	.pipe(cheerio({
-			run: function ($) {
-				$('[fill]').removeAttr('fill');
-				$('[stroke]').removeAttr('stroke');
-				$('[style]').removeAttr('style');
-			},
-			parserOptions: {xmlMode: true}
-	}))
+  gulp.src('app/assets/icons/svg/**/*.svg')
+  .pipe(svgmin({
+    js2svg: {
+      pretty: true
+    }
+  }))
+  // remove all fill, style and stroke declarations in out shapes
+  .pipe(cheerio({
+      run: function ($) {
+        $('[fill]').removeAttr('fill');
+        $('[stroke]').removeAttr('stroke');
+        $('[style]').removeAttr('style');
+      },
+      parserOptions: {xmlMode: true}
+  }))
 
-	// cheerio plugin create unnecessary string '&gt;', so replace it.
-	.pipe(replace('&gt;', '>'))
+  // cheerio plugin create unnecessary string '&gt;', so replace it.
+  .pipe(replace('&gt;', '>'))
 
-	// build svg sprite
-	.pipe(svgSprite({
-		mode: {
-			symbol: {
+  .pipe(plumber())
 
-				dest: '.',
-				bust: false,
-				sprite: 'sprite.svg',
-				layout: 'vertical',
-				render: {
-					scss: {
-						dest: 'svg-size.scss',
-						template: 'svg-sprite-template'
-					}
-				}
-
-			}
-		}
-	}))
-	.pipe(gulpIf('*.svg', gulp.dest('public/assets/img'), gulp.dest('app/styles/helpers')))
+  // build svg sprite
+  .pipe(svgSprite({
+    mode: {
+      symbol: {
+        dest: '.',
+        bust: false,
+        sprite: 'sprite.svg',
+        layout: 'vertical',
+        render: {
+          scss: {
+            dest: 'svg-size.scss',
+            template: 'svg-sprite-template'
+          }
+        }
+      }
+    }
+  }))
+  .pipe(gulpIf('*.svg', gulp.dest('public/assets/img'), gulp.dest('app/styles/helpers')))
 );
--- tasks/server.js
+++ tasks/server.js
@@ -1,35 +1,34 @@
 /* server */
-const gulp = require('gulp');
+require('gulp');
 const debuga = require('debuga');
 const browserSync = require('browser-sync').create();
 
-const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+const isDevelopment = !process.env.NODE_ENV
+  || process.env.NODE_ENV === 'development';
 
 module.exports = () => {
-	browserSync.init({
-    files: ['public/**/*'],
+  browserSync.init({
+    files: [
+      'public/**/*',
+    ],
     server: {
-			baseDir: [
-				'public'
-			],
-			directory: false,
-			middleware: isDevelopment ? [debuga({
-        favicon: './tasks/img/favicon.ico',
-        titlePrefix: 'TMPL — ',
-        dist: './public'
-      })] : []
-		},
-		tunnel: false,
+      baseDir: [
+        'public',
+      ],
+      directory: false,
+      middleware: isDevelopment ? [debuga()] : [],
+    },
+    tunnel: false,
     reloadOnRestart: true,
-		host: 'localhost',
-		port: 9000,
+    host: 'localhost',
+    port: 9000,
     snippetOptions: {
-			rule: {
-				match: /<\/body>/i
-			}
-		},
-    watchEvents : [ 'change', 'add', 'unlink', 'addDir', 'unlinkDir' ],
+      rule: {
+        match: /<\/body>/i,
+      },
+    },
+    watchEvents: ['change', 'add', 'unlink', 'addDir', 'unlinkDir'],
 
-	});
-	browserSync.watch('public/**/*.*').on('change', browserSync.reload)
+  });
+  browserSync.watch('public/**/*.*').on('change', browserSync.reload);
 };
