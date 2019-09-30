dd44e03fc30a4ee0469716c841482b52386bc181
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-09-30 19:05:31 +0300

    Big update to v3.0

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,26 @@
 import svg4everybody from 'svg4everybody';
 import mainMenu from '../blocks/main-menu/main-menu';
 
-window.onload = () => {
-  svg4everybody();
-  mainMenu();
-};
+class Viewer {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.initClasses();
+  }
+
+  initClasses() {
+    svg4everybody();
+    mainMenu();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Viewer();
+});
+
+// window.onload = () => {
+//   svg4everybody();
+//   mainMenu();
+// };
